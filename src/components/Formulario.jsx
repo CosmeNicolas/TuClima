02efdad688ff466cc961c7113e6abf3c73ccea8e
CosmeNicolas <nicomas7590@gmail.com>
2024-01-16 17:52:16 -0300

    cambie el estado del boton de submit a tipo boton y saque el setCiudad ya q no quiero actualizar el estado de ciudades por el momento

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,27 +4,37 @@ import {Form, Button}  from 'react-bootstrap'
 
 const Formulario = () => {
 
-  const [ciudades, setCiudades] = useState([])
-  console.log('desde el input')
+  const [ciudad, setCiudad] = useState([])
+  console.log(ciudad)
 
-  const handleForm =(e)=>{
-    setCiudades(e.target.value)
-    console.log(setCiudades)
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    console.log(ciudad)
+  }
+
+
+  const obtenerDatosApi = async () => {
+
+    const respuestaApiClima = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Tucuman&limit=5&appid=7a9a4933445cff553f7abda64a0617e6')
+    const datosApiclima = await (respuestaApiClima).json()
+    console.log(respuestaApiClima)
+    console.log(datosApiclima)
+
   }
 
 
   return (
-    <Form onChange={handleForm} className='mt-5'>
+    <Form onChange={handleSubmit} className='mt-5'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className='text-light'>Ingresa una Ciudad</Form.Label>
         <Form.Control
         type="text" 
         placeholder="Ingresa una ciudad" 
-        value={ciudades}
-        onChange={(e)=>setCiudades(e.target.value)}/>
+        value={ciudad}
+        onChange={(e)=>setCiudad(e.target.value)}/>
       </Form.Group>
       <div className='text-center'>
-      <Button  variant="primary" type="submit">
+        <Button onClick={obtenerDatosApi}  variant="primary" type="button">
         Consultar
       </Button>
       </div>
