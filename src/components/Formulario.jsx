d9a039fc466d8b63d6ab1286e0a2bda4262bd3b1
CosmeNicolas <nicomas7590@gmail.com>
2024-01-16 19:04:14 -0300

    hice del handleSubmit una promesa

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,24 +7,20 @@ const Formulario = () => {
   const [ciudad, setCiudad] = useState([])
   console.log(ciudad)
 
-  const handleSubmit =(e)=>{
+  const handleSubmit = async (e)=>{
     e.preventDefault()
     console.log(ciudad)
-  }
-
-
-  const obtenerDatosApi = async () => {
-
-    const respuestaApiClima = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Tucuman&limit=5&appid=7a9a4933445cff553f7abda64a0617e6')
+    const respuestaApiClima = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=7a9a4933445cff553f7abda64a0617e6`)
     const datosApiclima = await (respuestaApiClima).json()
     console.log(respuestaApiClima)
     console.log(datosApiclima)
-
   }
 
 
+ 
+
   return (
-    <Form onChange={handleSubmit} className='mt-5'>
+    <Form onSubmit={handleSubmit} className='mt-5'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className='text-light'>Ingresa una Ciudad</Form.Label>
         <Form.Control
@@ -34,7 +30,7 @@ const Formulario = () => {
         onChange={(e)=>setCiudad(e.target.value)}/>
       </Form.Group>
       <div className='text-center'>
-        <Button onClick={obtenerDatosApi}  variant="primary" type="button">
+        <Button  variant="primary" type="submit">
         Consultar
       </Button>
       </div>
