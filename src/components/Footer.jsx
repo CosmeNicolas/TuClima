81367741a10dead0156cc50719172601b4bc58c8
CosmeNicolas <nicomas7590@gmail.com>
2024-01-16 13:09:29 -0300

    agregue el componente footer y la carpeta components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <footer className='footer text-light text-center py-3'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
+  );
+};
+
+export default Footer;
