f3acf60e1591d3dce6997c0cf1274643239396bc
CosmeNicolas <nicomas7590@gmail.com>
2024-01-16 20:41:58 -0300

    agregue un div para poder centrar la card cuando consulte la ciudad

--- src/components/CardFormulario.jsx
+++ src/components/CardFormulario.jsx
@@ -1,5 +1,6 @@
 import {Card, ListGroup, Image,Alert}  from 'react-bootstrap'
-import nubes from '../assets/nubes.jpg'
+import nubes from '../assets/nube.gif'
+import lluvia from '../assets/lluvia.gif'
 
 
 const CardFormulario = ({datosClima}) => {
@@ -10,24 +11,28 @@ const CardFormulario = ({datosClima}) => {
   const redondearTemperatura = (temperatura)=>{
     return Math.round(temperatura / 10)
   }
+
+  const verlluvia = datosClima.main.humidity > 90
  
 
   return (
     <>
-      <Card className='my-3 cards'>
-        <Image src={nubes} className='img-fluid'/>
+      <div className='mt-4 div-contenedor-card'>
+        <Card className='my-3 contenedor-card' >
+          <Image src={verlluvia ? lluvia : nubes} className='img-fluid cards'/>
         <Card.Body>
           <Card.Title>{datosClima.name}</Card.Title>
           <ListGroup variant="flush">
             <ListGroup.Item>Humedad: {datosClima.main.humidity} % </ListGroup.Item>
             <ListGroup.Item>Presion: {datosClima.main.pressure} Pa</ListGroup.Item>
-            <ListGroup.Item>Presion: {datosClima.wind.speed} km/h</ListGroup.Item>
+            <ListGroup.Item>Viento: {datosClima.wind.speed} km/h</ListGroup.Item>
             <ListGroup.Item>Sensación Termica: {redondearTemperatura(datosClima.main.feels_like)}°C</ListGroup.Item>
             <ListGroup.Item>Temperatura Minima: {redondearTemperatura(datosClima.main.temp_min)}°C</ListGroup.Item>
             <ListGroup.Item>Temperatura Maxima: {redondearTemperatura(datosClima.main.temp_max)}°C</ListGroup.Item>
           </ListGroup>
         </Card.Body>
       </Card>
+      </div>
     </>
   )
 }
