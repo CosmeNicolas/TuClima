4239fb2c690b36f6ba38712234f7a6b7f9cf3567
CosmeNicolas <nicomas7590@gmail.com>
2024-01-16 17:33:23 -0300

    agregue el manejador de eventos al ingresar datos en el input del proyecto

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
 import {Form, Button}  from 'react-bootstrap'
 
 
 const Formulario = () => {
+
+  const [ciudades, setCiudades] = useState([])
+  console.log('desde el input')
+
+  const handleForm =(e)=>{
+    setCiudades(e.target.value)
+    console.log(setCiudades)
+  }
+
+
   return (
-    <Form className='mt-5'>
+    <Form onChange={handleForm} className='mt-5'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className='text-light'>Ingresa una Ciudad</Form.Label>
-        <Form.Control type="email" placeholder="Ingresa una ciudad" />
+        <Form.Control
+        type="text" 
+        placeholder="Ingresa una ciudad" 
+        value={ciudades}
+        onChange={(e)=>setCiudades(e.target.value)}/>
       </Form.Group>
       <div className='text-center'>
       <Button  variant="primary" type="submit">
