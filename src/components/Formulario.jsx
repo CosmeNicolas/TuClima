4a1ddc6fb2727732caba60b1d2ee6267beab6997
CosmeNicolas <nicomas7590@gmail.com>
2024-01-16 21:34:13 -0300

    agregue un spinner junto con el manejo de errores con try y catch

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,21 +1,41 @@
 import { useState } from 'react'
-import {Form, Button}  from 'react-bootstrap'
+import {Form, Button, Alert , Spinner}  from 'react-bootstrap'
 import CardFormulario from './CardFormulario'
 
 
 const Formulario = () => {
 
-  const [ciudad, setCiudad] = useState([])
+  const [ciudad, setCiudad] = useState('')
   const [datosClima, setDatosClima] = useState(null)
+  const [cargando, setCargando] = useState(false)
+  const [error, setError] = useState(null)
   console.log(ciudad)
 
   const handleSubmit = async (e)=>{
     e.preventDefault()
-    const respuestaApiClima = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=7a9a4933445cff553f7abda64a0617e6`)
-    const datosClima = await (respuestaApiClima).json()
-    setDatosClima(datosClima)
+   
     console.log(datosClima)
-    setCiudad('')
+    // Validar que la ciudad no esté vacía
+    if (!ciudad.trim()) {
+      setError('Por favor, ingresa el nombre de una ciudad.');
+      return;
+    }
+
+    try {
+      // Iniciar la carga
+      setCargando(true);
+
+      const respuestaApiClima = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=7a9a4933445cff553f7abda64a0617e6`)
+      const datosClima = await (respuestaApiClima).json()
+      setDatosClima(datosClima)
+      setCargando(false)
+      setError(null)
+    } catch (error) { 
+      //manejar los errores 
+      console.log('error al consulatr', error);
+      setCargando(false)
+      setError('Hubo un error al consultar el clima. Por favor, intenta nuevamente.');
+    }
   }
 
 
@@ -33,11 +53,14 @@ const Formulario = () => {
         onChange={(e)=>setCiudad(e.target.value)}/>
       </Form.Group>
       <div className='text-center'>
-        <Button  variant="primary" type="submit">
-        Consultar
+          <Button variant="primary" type="submit" disabled={cargando}>
+            {cargando ? <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" /> : 'Consultar'}
       </Button>
       </div>
     </Form>
+
+      {error && <Alert variant='danger' className='mt-3'>{error}</Alert>}
+
       <CardFormulario datosClima={datosClima} />
     </>
   )
