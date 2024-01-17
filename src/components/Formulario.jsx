ed8698f2b97f2ac26da9a7a369b37db7b093f3cb
CosmeNicolas <nicomas7590@gmail.com>
2024-01-16 21:53:11 -0300

    agregue una nueva validacion por un error en la api q estaba teniendo al consultar la ciudad

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -23,8 +23,9 @@ const Formulario = () => {
     try {
       // Iniciar la carga
       setCargando(true);
-
-      const respuestaApiClima = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=7a9a4933445cff553f7abda64a0617e6`)
+      // Modificar la consulta para incluir el país si está presente
+      const consulta = codigoPais ? `${ciudad},${codigoPais}` : ciudad;
+      const respuestaApiClima = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${consulta}&appid=7a9a4933445cff553f7abda64a0617e6`)
       const datosClima = await (respuestaApiClima).json()
       setDatosClima(datosClima)
       setCargando(false)
