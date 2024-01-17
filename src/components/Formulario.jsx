1f85ee059eddb39bf5a0158e094e9d38d341af49
CosmeNicolas <nicomas7590@gmail.com>
2024-01-16 21:37:31 -0300

    agregue una direccion https

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -25,7 +25,7 @@ const Formulario = () => {
       // Iniciar la carga
       setCargando(true);
 
-      const respuestaApiClima = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=7a9a4933445cff553f7abda64a0617e6`)
+      const respuestaApiClima = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=7a9a4933445cff553f7abda64a0617e6`)
       const datosClima = await (respuestaApiClima).json()
       setDatosClima(datosClima)
       setCargando(false)
